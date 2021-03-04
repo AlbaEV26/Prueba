ffda1d0014a61e5ad8899f7b93740c90b1a439f2
AlbaEV26 <4110702@alu.murciaeduca.es>
2021-03-04 11:23:54 +0100

    Cambio en Eclipse

--- main.java
+++ main.java
@@ -2,5 +2,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Prueba en java");
+		
+		
+		System.out.println("Añadimos comentarios en Eclipse y comprobamos que aparecen en Github");
 	}
 }
